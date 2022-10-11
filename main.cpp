467a31ceccab72e5757718bbab8df232ca5f5fac
TeddyRedy <kataich75@gmail.com>
2022-10-11 22:10:13 +0300

    GitIgnore minor update. Version 1.6

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include "crc.h"
 using namespace std;
 
 int main(){
     string str("CE4E879366B8C328");
-	cout << str << endl;
+	cout << CRC64(str,str.size()) << endl;
+	crc
 	return 0;
 }
