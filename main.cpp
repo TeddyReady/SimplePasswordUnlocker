d73c80c599969578813490cbd08aa6fe5e33019b
TeddyRedy <kataich75@gmail.com>
2022-10-11 22:41:09 +0300

    CRC64 lib update. Version 1.8

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include "crc.h"
 using namespace std;
 
 int main(){
-    string str("CE4E879366B8C328");
-	cout << CRC64(str,str.size()) << endl;
-	crc
+    const char* str("HelloWorld");
+	cout << crc64(str, strlen(str)) << endl;
 	return 0;
 }
