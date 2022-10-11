aaa3cc085be00c91e3eb3b57aeeb6056e54f89a2
TeddyRedy <kataich75@gmail.com>
2022-10-11 23:29:42 +0300

    Added chrono support. Version 3.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <chrono>
 #include "crypto.h"
 using namespace std;
+using namespace std::chrono;
 
 int main(){
-    const char* str("BYaKa");
+	auto start = high_resolution_clock::now();
+
+    const char* str("12345");
 
 	cout << "Your Password is " << passTable(crc64(str, strlen(str))) << endl;
+	
+	auto finish = high_resolution_clock::now();
+	cout << "The program works ... " << duration_cast<microseconds>( finish - start ).count() << " micro seconds" << endl;
 	return 0;
 }
